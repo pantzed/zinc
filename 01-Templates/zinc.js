a5cfa583a18ad75e14e50829c983e892fb50de7b
Edward <edpantzar@gmail.com>
2018-07-31 11:12:44 -0500

    Completed step #2

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -4,6 +4,18 @@
 
 (() => {
     function populateList(results) {
+        results.forEach((user) => {
+            let newListItem = document.createElement('li');
+            newListItem.classList.add('user');
+            let newListItemContents = `
+                <img class="user-photo" src="${user.picture.thumbnail}" alt="Photo of ${user.name.first} ${user.name.last}">
+                <div class="user-name">${user.name.first} ${user.name.last}</div>
+                <div class="user-location">${user.location.city}, ${user.location.state}</div>
+                <div class="user-email">${user.email}</div>
+            `;
+            newListItem.innerHTML = newListItemContents;
+            document.getElementById('z-user-list').appendChild(newListItem);
+        });
         console.log(results); // eslint-disable-line no-console
     }
 
