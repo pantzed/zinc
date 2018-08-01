03f376ed280debdc22417265ba9bbf0954461293
Edward <edpantzar@gmail.com>
2018-08-01 14:18:58 -0500

    #4

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -6,30 +6,24 @@
 (() => {
 
     function renderTemplate(template, users) {
-        let bracketStuff = /\{\{\s+([\w.]+)\s+\}\}/gm;
-        
-        users.forEach((user) => {
-            renderTemplate = template.replace(bracketStuff, (match, matches) => {
-                let arr = matches.split('.');
-                return arr.reduce((acc, curr) => acc[curr], user);
+        fetch(`${template}.html`)
+            .then(template => template.text())
+            .then((template) => {
+                let bracketStuff = /{{\s*([\w.]+)\s*}}/g;
+                users.forEach((user) => {
+                    let renderTemplate = template.replace(bracketStuff, (match, matches) => {
+                        let arr = matches.split('.');
+                        return arr.reduce((acc, curr) => acc[curr], user);
+                    });
+                    document.getElementById('z-user-list').insertAdjacentHTML('beforeend', renderTemplate);
+                });
             });
-            document.getElementById('z-user-list').insertAdjacentHTML('beforeend', renderTemplate);
-        });
-    }
-
-    const userLiTemplate = `
-    <li class="user">
-        <img class="user-photo" src="{{ picture.thumbnail }}" alt="Photo of {{ name.first }} {{ name.last }}">
-        <div class="user-name">{{ name.first }} {{ name.last }}</div>
-        <div class="user-location">{{ location.city }}, {{ location.state }}</div>
-        <div class="user-email">{{ email }}</div>
-    </li>
-    `;
+    };
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
             .then(res => res.json())
-            .then(data => renderTemplate(userLiTemplate, data.results));
+            .then(data => renderTemplate('user', data.results));
     }
 
     document.addEventListener('DOMContentLoaded', init);
