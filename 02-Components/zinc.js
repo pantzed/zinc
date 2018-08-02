10c94b4e6d91ee1038a885a13019de96f388ee0d
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-02 15:31:00 -0500

    Refactored #2

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -3,39 +3,41 @@
 /* eslint-env browser */
 
 const Zinc = {};
-Zinc.components = [];
+Zinc.components = {};
 Zinc.registerComponent = function(elementName, templateFile, dataObject){
-    Zinc.components.push({
+    Zinc.components[elementName] = {
         elementName,
         templateFile,
         dataObject
-    });
+    };
 };
 
 (() => {
     function renderComponents(components){
-        components.forEach((component) => {
-            renderComponent(component.elementName, component.templateFile, component.dataObject);
-        })
+        for (let component in components) {
+            renderComponent(
+                components[component].elementName,
+                components[component].templateFile,
+                components[component].dataObject)
+        }
     }
 
     function renderComponent(element, content, data) {
-        const parent =  document.getElementsByTagName(element)[0];
+        const elements = Array.from(document.getElementsByTagName(element));
         fetch(`${content}.html`)
         .then(html => html.text())
         .then(template => {
             let regex = /{{\s*([\w.]+)\s*}}/g;
             let arr = [data];
             arr.forEach(user => 
-                parent.insertAdjacentHTML('beforeend', template.replace(regex, (match, capture) => 
-                    capture.split('.').reduce((acc, curr) =>
-                        acc[curr], user))));
+                elements.forEach((element) => 
+                    element.insertAdjacentHTML('beforeend', template.replace(regex, (match, capture) => 
+                        capture.split('.').reduce((acc, curr) =>
+                            acc[curr], user)))));
         });
     }
 
     function init() {
-        Zinc.registerComponent('user-item', 'user', Zinc.userData);
-        Zinc.registerComponent('user-item', 'user', Zinc.userData);
         Zinc.registerComponent('user-item', 'user', Zinc.userData);
         renderComponents(Zinc.components);
     }
