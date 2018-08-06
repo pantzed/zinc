fa4e17c71117623e96639ac7aebec28d0d30d4f0
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-06 13:57:32 -0500

    Refactored how controllers are added

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -4,12 +4,12 @@
 
 const Zinc = {};
 Zinc.components = {};
-Zinc.registerComponent = function(componentObject){
+Zinc.registerComponent = function (componentObject) {
     Zinc.components[componentObject.elementName] = componentObject;
 };
 
 (() => {
-    function renderComponents(components){
+    function renderComponents(components) {
         for (let component in components) {
             renderComponent(components[component]);
         }
@@ -18,31 +18,32 @@ Zinc.registerComponent = function(componentObject){
     function renderComponent(component) {
         const elements = Array.from(document.getElementsByTagName(component.elementName));
         fetch(`${component.templateFile}.html`)
-        .then(html => html.text())
-        .then(template => {
-            let tagMatch = /{{\s*([\w.]+)\s*}}/g;
-            let arr = [component.data];
-            arr.forEach(user => 
-                elements.forEach((element) => {
-                    element.addEventListener('click', component.controller);
-                    element.insertAdjacentHTML('beforeend', template.replace(tagMatch, (match, capture) => 
-                        capture.split('.').reduce((acc, curr) =>
-                            acc[curr], user)));
-                            checkForNested(element);
-                }));
-        });
+            .then(html => html.text())
+            .then(template => {
+                let tagMatch = /{{\s*([\w.]+)\s*}}/g;
+                let arr = [component.data];
+                arr.forEach(user =>
+                    elements.forEach((element) => {
+                        element.insertAdjacentHTML('beforeend', template.replace(tagMatch, (match, capture) =>
+                            capture.split('.').reduce((acc, curr) =>
+                                acc[curr], user)));
+                        checkForNested(element);
+                        if (component.controller !== undefined) {
+                            element.controller = component.controller;
+                            element.controller();
+                        }
+                    }));
+            });
     }
 
     function checkForNested(parentNode) {
         if (parentNode.childNodes.length === 0) {
             return;
-        }
-        else {
+        } else {
             Array.from(parentNode.childNodes).forEach((node) => {
                 if (Zinc.components[node.localName] !== undefined) {
                     renderComponent(Zinc.components[node.localName]);
-                }
-                else {
+                } else {
                     checkForNested(node);
                 }
             });
@@ -51,28 +52,31 @@ Zinc.registerComponent = function(componentObject){
     }
 
     function toggleHilight() {
-        console.log(this);
-        console.log(this.firstElementChild);
-        this.firstElementChild.classList.toggle('hilight');
+        this.addEventListener('click', () => {
+            event.currentTarget.firstElementChild.classList.toggle('hilight');
+        });
     }
 
     function init() {
         fetch('https://randomuser.me/api/?results=5')
-        .then(res => res.json())
-        .then(data => {
-            for(let i=0; i<data.results.length; i++){
+            .then(res => res.json())
+            .then(data => {
+                for (let i = 0; i < data.results.length; i++) {
+                    Zinc.registerComponent({
+                        elementName: `user${i}`,
+                        templateFile: 'user',
+                        data: data.results[i],
+                        controller: toggleHilight
+                    });
+                }
+            })
+            .then(() => {
                 Zinc.registerComponent({
-                    elementName: `user${i}`,
-                    templateFile: 'user',
-                    data: data.results[i],
-                    controller: toggleHilight
-                });
-            }
-        })
-        .then(() => {
-            Zinc.registerComponent({elementName: 'user-list', templateFile:'user-list'})
-            return renderComponents(Zinc.components)
-        });
+                    elementName: 'user-list',
+                    templateFile: 'user-list'
+                })
+                return renderComponents(Zinc.components)
+            });
     }
 
     document.addEventListener('DOMContentLoaded', init);
